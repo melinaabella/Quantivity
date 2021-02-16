ac70fc7d6de096a38e7d87d446977c5391bb3821
Topherwa <cbrossard@nevada.unr.edu>
2021-02-16 10:21:54 -0800

    changed 'client' folder to 'proxy'

--- server/routes/about.js
+++ server/routes/about.js
@@ -2,33 +2,9 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-//promise wrapper for the readFile function
-const readFile = (fileName, encoding) => {
-	return new Promise((resolve, reject) => {
-		fs.readFile(fileName, encoding, (err, data) => {
-			if (err) {
-				return reject(err);
-			} else {
-				resolve(data);
-			}
-		})
-	});
-};
-
 //Get about page
 router.get('/', (req, res, next) => {
-
 	res.sendFile(path.join(__dirname + '/../public/html/about.html'));
-	/*
-	readFile(__dirname + '/../public/html/about.html').then(contents => {
-		res.send(contents);
-	}).catch(err => {
-		console.log(`Unable to read /about.html: ${err}`);
-		res.status(500);
-		res.send(err);
-		return;
-	});
-	*/
 });
 
 module.exports = router;
